c126d9d2ad9c50522e3e23529d7149154f9883b2
Norbert Stüken <norbert.stueken@nrbrt.com>
2020-12-24 10:51:54 +0100

    emacs: Install evil mode automatically

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (htmlize use-package org-journal markdown-mode helm evil-visual-mark-mode evil-surround evil-leader evil-indent-textobject))))
+ '(package-selected-packages (quote (evil use-package))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
